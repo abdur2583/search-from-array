dcca34caf19dc95c26f9bc6c9931416bf44c40b7
Abdur Rahman <44979323+abdur2583@users.noreply.github.com>
2020-05-19 17:15:30 +0600

    Create search.js

---
+++ search.js
@@ -0,0 +1,9 @@
+var myTable = ["pencil","pen","books","paper"];
+
+for(var i = 0; i<myTable.length; i++){
+  var eachThing = myTable[i];
+    if (eachThing= "pen"){
+      console.log("Found pen");
+       break;
+  }
+}
